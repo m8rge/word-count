7c075218b1f6c67641719cf73ee288286f1d1b16
aputilov <to.merge@gmail.com>
2018-12-06 15:30:00 +0500

    revise

--- src/CountCommand.php
+++ src/CountCommand.php
@@ -9,20 +9,22 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CountCommand
 {
     /**
-     * @var string[]
+     * @var array [word => appearanceCountInt, ...]
      */
     private $words = [];
     
-    function __invoke(InputInterface $input, OutputInterface $output)
+    public function __invoke(InputInterface $input, OutputInterface $output)
     {
         $filename = $input->getArgument('filename');
         if (!file_exists($filename)) {
             throw new RuntimeException("File $filename doesn't exists");
-        } elseif (!is_file($filename)) {
+        }
+
+        if (!is_file($filename)) {
             throw new RuntimeException("$filename isn't a file");
         }
 
-        $f = fopen($filename, 'r');
+        $f = fopen($filename, 'rb');
         if (!$f) {
             throw new RuntimeException("Can't open $filename");
         }
@@ -35,11 +37,13 @@ class CountCommand
             if ($lastSpace > 0) {
                 $part = substr($buffer, 0, $lastSpace);
                 $buffer = substr($buffer, $lastSpace+1);
-                $this->addWords($part);
+                $wordsCount = $this->countWords($part);
+                $this->addWords($wordsCount);
             }
         }
 
-        $this->addWords($buffer);
+        $wordsCount = $this->countWords($buffer);
+        $this->addWords($wordsCount);
 
         array_multisort(array_values($this->words), SORT_DESC, array_keys($this->words), SORT_ASC, $this->words);
         foreach ($this->words as $word => $count) {
@@ -47,22 +51,23 @@ class CountCommand
         }
     }
 
-    /**
-     * @param string $string
-     */
-    private function addWords($string)
+    private function addWords(array $wordsCount): void
+    {
+        foreach ($wordsCount as $word => $count) {
+            if (array_key_exists($word, $this->words)) {
+                $this->words[$word] += $count;
+            } else {
+                $this->words[$word] = $count;
+            }
+        }
+    }
+
+    private function countWords(string $string): array
     {
         preg_match_all('/\p{L}+/u', $string, $matches);
-        $blockWords = array_map('mb_strtolower', $matches[0]);
-        $blockWords = array_count_values($blockWords);
+        $wordsCount = array_map('mb_strtolower', $matches[0]);
+        $wordsCount = array_count_values($wordsCount);
 
-        // add existing words
-        array_walk($this->words, function (&$value, $word) use ($blockWords) {
-            if (array_key_exists($word, $blockWords)) {
-                $value += $blockWords[$word];
-            }
-        });
-        // add new words
-        $this->words += $blockWords;
+        return $wordsCount;
     }
 }
